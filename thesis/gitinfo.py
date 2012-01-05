d023b1689608a441dead249377014fbee70f9749
Kimmo Puputti <kpuputti@gmail.com>
2012-01-05 14:59:54 +0200

    Add script to LaTeX build process to inject Git repo info to the document.

---
+++ thesis/gitinfo.py
@@ -0,0 +1,32 @@
+import subprocess
+import sys
+
+
+TMPL='''\\section{{Thesis Git repository info}}
+
+\\textbf{{Git HEAD:}}
+
+\\begin{{verbatim}}
+{head}\\end{{verbatim}}
+
+\\noindent \\textbf{{Repository status:}}
+\\begin{{verbatim}}
+{status}\\end{{verbatim}}
+'''
+
+
+def githead():
+    return subprocess.check_output(['git', 'log', '-n' '1'])
+
+
+def gitstatus():
+    return subprocess.check_output(['git', 'status'])
+
+
+def main():
+    output = TMPL.format(head=githead(), status=gitstatus())
+    print(output)
+
+
+if __name__ == '__main__':
+    sys.exit(main() or 0)
